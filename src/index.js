d06dab91c1a9de6f87a532e835f8208a24295380
reddyadi2020 <32670191+reddyadi2020@users.noreply.github.com>
2022-07-11 12:48:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://server-catstronaut.herokuapp.com/',
+  uri: 'https://server-test5.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
